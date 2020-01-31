6d82aa2f35b8f70a052cd5d22558f3fd061a37da
Kenneth V. Domingo <kvdomingo@up.edu.ph>
2020-01-31 16:23:13 +0800

    added readme.md

---
+++ README.md
@@ -0,0 +1,10 @@
+# PrimerX - Automated design of mutagenic primers for site-directed mutagenesis
+
+## Requirements
+- Python 3.6
+
+## Command line tool
+You can access and download the command line code [here](sdm/primerx.py)
+
+## Web application
+The updated website can be accessed via https://primerx.herokuapp.com/.
--- sdm/primerx.py
+++ sdm/primerx.py
@@ -3,27 +3,53 @@ from pandas import DataFrame
 
 
 bases_dict = {
-    'T': 'Thymine',
-    'A': 'Adenina',
-    'G': 'Guanine',
-    'C': 'Cytosine',
+    'T': 'thymine',
+    'A': 'adenine',
+    'G': 'guanine',
+    'C': 'cytosine'
 }
 
 complement_dict = {
     'T': 'A',
     'A': 'T',
     'G': 'C',
-    'C': 'G',
+    'C': 'G'
+}
+
+protein_dict = {
+    'A': 'alanine',
+    'R': 'arginine',
+    'N': 'asparagine',
+    'D': 'aspartic acid',
+    'C': 'cysteine',
+    'Q': 'glutamine',
+    'E': 'glutamic acid',
+    'G': 'glycine',
+    'H': 'histidine',
+    'I': 'isoleucine',
+    'L': 'leucine',
+    'K': 'lysine',
+    'M': 'methionine',
+    'F': 'phenylalanine',
+    'P': 'proline',
+    'S': 'serine',
+    'T': 'threonine',
+    'W': 'tryptophan',
+    'Y': 'tyrosine',
+    'V': 'valine'
 }
 
 
 class PrimerDesign:
-    def __init__(self, sequence, mutation_type, destination, position, target=None):
+    def __init__(self, mode, sequence, mutation_type, destination, position, target=None):
+        self.mode = mode
         self.sequence = sequence
         self.mutation_type = mutation_type.lower()
         self.target = target
         self.destination = destination
         self.position = position
+
+        self.checks()
 
         if self.mutation_type in ['s', 'sub']:
             self.substitution()
@@ -33,6 +59,13 @@ class PrimerDesign:
             self.deletion()
         else:
             raise NotImplementedError
+
+    def checks(self):
+        if self.mode.lower() == 'dna':
+            unique_bases = set(list(self.sequence))
+            true_bases = {'A', 'C', 'T', 'G'}
+            if len(unique_bases.union(true_bases)) != 4:
+                raise ValueError('DNA sequence contains invalid bases.')
 
     def substitution(self):
         seq = list(self.sequence)
@@ -61,6 +94,7 @@ def main():
     parser.add_argument('-t', '--target', help='Target base', type=str)
     parser.add_argument('-d', '--destination', help='Replacement for target base', type=str)
     parser.add_argument('-p', '--position', help='Target base position', type=int)
+    parser.add_argument('--mode', help="Choose between `dna` or `pro` mode", type=str)
     parser.add_argument('-i', '--interactive', help='Interactive mode', action='store_true')
     args = parser.parse_args()
     args_dict = dict()
@@ -72,18 +106,20 @@ def main():
         print('====      PrimerX v0.1.1       ====')
         print('====                           ====')
         print('===================================\n')
+        args_dict['mode'] = input('Enter primer mode [dna/protein]: ')
         args_dict['sequence'] = input('Enter DNA sequence: ')
         args_dict['mutation_type'] = input('Enter mutation type [s/i/d]: ')
-        if args_dict['mutation_type'].lower() == 's':
+        if args_dict['mutation_type'].lower() in ['s', 'sub']:
             args_dict['target'] = input('Enter target base: ')
         else:
             args_dict['target'] = None
         args_dict['destination'] = input('Enter replacement for target base: ')
         args_dict['position'] = int(input('Enter position of target: '))
     else:
+        args_dict['mode'] = args.mode
         args_dict['sequence'] = args.sequence
         args_dict['mutation_type'] = args.mutation_type
-        if args_dict['mutation_type'].lower() == 's':
+        if args_dict['mutation_type'].lower() in ['s', 'sub']:
             args_dict['target'] = args.target
         else:
             args_dict['target'] = None
@@ -94,7 +130,7 @@ def main():
     gccont = (res.out.count('G') + res.out.count('C'))/len(res.out)
     idx = ['Forward', 'Reverse', 'GC content', 'Length']
     revcomp = ''.join([complement_dict[b] for b in list(res.out[::-1])])
-    dat = [res.out, revcomp, f'{gccont*100}%', f'{len(res.out)} bp']
+    dat = [res.out, revcomp, f'{gccont*100:.2f}%', f'{len(res.out)} bp']
     df = DataFrame(
         data=dat,
         index=idx,
