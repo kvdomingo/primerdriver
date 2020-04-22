ceaec38f52d1729c9aa59f7e84636098e2daaebe
Kenneth V. Domingo <kvdomingo@up.edu.ph>
2020-04-22 19:11:56 +0800

    add browser-tools orb

--- sdm/snapshot.js
+++
@@ -1,7 +0,0 @@
-const percyScript = require('@percy/script');
-
-PercyScript.run(async (page, percySnapshot) => {
-    await page.goto('http://localhost:8000');
-    await page.waitFor('#app');
-    await percySnapshot('homepage');
-});
