262f718facdfb55161c2dbdc0c9f338a2f4c47dc
Kenneth V. Domingo <kvdomingo@up.edu.ph>
2020-04-22 19:44:05 +0800

    detach database

--- primerx/settings.py
+++ primerx/settings.py
@@ -97,12 +97,7 @@ WSGI_APPLICATION = 'primerx.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+DATABASES = {}
 
 
 # Password validation
