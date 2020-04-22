3843ba56a3fadcd650c50767e3d4fe78604c9dd4
Kenneth V. Domingo <kvdomingo@up.edu.ph>
2020-04-22 19:56:27 +0800

    remove selenium tests

--- sdm/tests.py
+++ sdm/tests.py
@@ -5,12 +5,12 @@ from django.test import TestCase
 from django.conf import settings
 
 
-class LandingTestCase(TestCase):
-    options = Options()
-    options.headless = True
-    browser = webdriver.Firefox(options=options)
-    browser.get('http://127.0.0.1:8000')
-    browser.implicitly_wait(10)
-    browser.execute_script('window.scrollTo(100, Y)')
-    percySnapshot(browser=browser, name='homepage')
-    browser.close()
+# class LandingTestCase(TestCase):
+#     options = Options()
+#     options.headless = True
+#     browser = webdriver.Firefox(options=options)
+#     browser.get('http://127.0.0.1:8000')
+#     browser.implicitly_wait(10)
+#     browser.execute_script('window.scrollTo(100, Y)')
+#     percySnapshot(browser=browser, name='homepage')
+#     browser.close()
