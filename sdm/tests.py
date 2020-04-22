5a3b9371a57bc20b47fc4b6325cc62ca29aec763
Kenneth V. Domingo <kvdomingo@up.edu.ph>
2020-04-22 16:16:57 +0800

    add circleci badge to readme

--- sdm/tests.py
+++ sdm/tests.py
@@ -1,6 +1,24 @@
+import os
+from pdcli.primerclass import PrimerDesign
 from django.test import TestCase
 
 
-class DummyTestCase(TestCase):
-    def test_dummy(self):
-        self.assertEqual(1, 1)
+class CommandLineTests(TestCase):
+    def test_return_help_on_no_arguments(self):
+        self.assertEqual(os.system('python pdcli.py'), 1)
+
+    def test_no_exception_on_help(self):
+        self.assertEqual(os.system('python pdcli.py -h'), 0)
+
+
+class CharacterizeTests(TestCase):
+    pd = PrimerDesign('char', 'GATTACA', 'sub', 1, 'T', 'A', 4)
+
+    def test_gc_content(self):
+        self.assertAlmostEqual(self.pd.calculate_gc_content(self.pd.sequence), 0.286, 3)
+
+    def test_calculate_mismatch(self):
+        self.assertAlmostEqual(
+            self.pd.calculate_mismatch(self.pd.sequence, self.pd.mismatched_bases),
+            1/7
+        )
