418e668fc4064f3312b39fd0c6dc564e9c74dab1
Kenneth V. Domingo <kvdomingo@up.edu.ph>
2020-04-22 19:51:56 +0800

    reattach database and invert debug env var

--- primerx/settings.py
+++ primerx/settings.py
@@ -97,7 +97,12 @@ WSGI_APPLICATION = 'primerx.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
-DATABASES = {}
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 
 # Password validation
